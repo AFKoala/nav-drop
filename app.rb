04dee0c964730c21a21a4edc688039a604b352d3
AFKoala <bubbles.murph@gmail.com>
2017-08-08 08:58:04 -0400

    added coin changer

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require_relative 'coins.rb'
 
 get '/' do
 	erb :index
+end
+
+get '/coins' do
+	erb :coins
+end
+
+post '/coins' do
+    cents_given_to_me = params[:cents_given_to_me]
+    coins_given_to_customer = params[:coins_given_to_customer]
+    coins_given_to_customer = coin_changer(cents_given_to_me)
+    redirect '/results?cents_given_to_me=' + cents_given_to_me + '&coins_given_to_customer=' + coins_given_to_customer
+end
+
+get '/results' do
+    cents_given_to_me = params[:cents_given_to_me]
+    coins_given_to_customer = params[:coins_given_to_customer]
+    erb :results, :locals => {:cents_given_to_me => cents_given_to_me, :coins_given_to_customer => coins_given_to_customer}
 end
---
+++ coins.rb
@@ -0,0 +1,12 @@
+def coin_changer(cents_given_to_me)
+  coin_value = {quarter: 25, dime: 10, nickel: 5, penny: 1}
+  coins_given_to_customer = {quarter: 0, dime: 0, nickel: 0, penny: 0}
+
+  coin_value.each do |coins, coin_worth|
+    while cents_given_to_me.to_i >= coin_worth.to_i
+      cents_given_to_me = cents_given_to_me.to_i - coin_worth.to_i
+      coins_given_to_customer[coins] += 1
+    end
+  end
+  coins_given_to_customer.to_s
+end
